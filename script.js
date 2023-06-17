3eb8a36c23ae0ea2cd42831930cdbd28459fae6f
Haboglabotribin <jkupton@icloud.com>
2023-06-17 20:02:37 +1000

    Adds centered n*n grid that grows via n=sqrt of sqaures in grid (square grid).

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let boxcount = 1;
 
 function drawGrid(){
     spawnGrid.addEventListener('click', () => {
+        createGrid()
         clearClones(container);
         for (let i = 0; i < boxcount**2; i++){
             container.append(div.cloneNode()); 
@@ -18,6 +19,19 @@ function drawGrid(){
 function clearClones(parent){
     while (parent.firstChild){
         parent.removeChild(parent.firstChild);
+    }
+}
+
+function createGrid(){
+    
+    container.style.gridTemplateColumns = `repeat(${boxcount}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${boxcount}, 1fr)`;
+    
+    for (let i = 1; i <= boxcount * boxcount; i++) {
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid-item');
+      gridItem.textContent = i;
+      container.appendChild(gridItem);
     }
 }
 
